d95de6f31cc93b0bc402ae8af04429debd6cea1b
José Carmona Cervera <38842510+Pecarcer@users.noreply.github.com>
2021-03-02 16:49:05 +0100

    tabla con info de api adecentada

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,33 +8,66 @@
     <div class="py-12">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="inicio"> 
-                <div class="p-4 bg-white border-b border-gray-200" >
-                <img class="image-container" src={{asset("storage/welcome.jpg")}} />
-                </div>
+                <div class="inicio">
+                    <div class="p-4 bg-white border-b border-gray-200">
+                        <img class="image-container" src={{asset("storage/welcome.jpg")}} />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    
-    
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Título</th>
-      <th scope="col">Nota</th>
-      <th scope="col">Estreno</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($movieReviews["results"] as $review)
-     <tr>
-    <td>{{ $review["original_title"] }}</td>
-    <td>{{ $review["vote_average"] }}</td>
-    <td>{{ $review["release_date"] }}</td>
-    </tr>
- @endforeach
-  </tbody>
-</table>
+    <div class="row">
+
+        <div class="col-md-12 text-center">
+            <h2> Películas Populares según The Movie Database </h2>
+        </div>
+    </div>
+
+    <div class="overflow-x-auto">
+        <div class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+            <div class="w-full lg:w-5/6">
+                <div class="bg-white shadow-md rounded my-6">
+                    <table class="min-w-max w-full table-auto">
+                        <thead>
+                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                <th class="py-3 px-6 text-left">Titulo</th>
+                                <th class="py-3 px-6 text-left">Nota Media</th>
+                                <th class="py-3 px-6 text-center">Fecha Estreno</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-gray-600 text-sm font-light">
+
+                            @foreach($movieReviews["results"] as $review)
+
+
+
+                            <tr class="border-b border-gray-200 hover:bg-gray-100">
+
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <span class="font-medium">{{$review["original_title"]}}</span>
+                                    </div>
+                                </td>
+
+                                <td class="py-3 px-6 text-left">
+                                    <div class="flex items-center">
+
+                                        <span>{{$review["vote_average"]}}</span>
+                                    </div>
+                                </td>
+
+                                <td class="py-3 px-6 text-center">
+                                    <div class="flex items-center justify-center">
+                                        <span>{{$review["release_date"]}}</span>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
